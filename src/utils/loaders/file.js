565230d3379c8a909ea6b19688a09844988103c5
Freddy Rodriguez <cristinamar2000@gmail.com>
2015-07-21 09:58:52 -0430

    fil util test and file util change to class

--- src/utils/loaders/file.js
+++ src/utils/loaders/file.js
@@ -1,35 +1,52 @@
-'use strict';
-
 import path from 'path';
 import yaml from 'js-yaml';
 import fs from 'fs';
 
-// TODO: change to class with constructor for folder, name, ext!
-export default {
-  nameFor: function(name, ext) {
-    return `${name}.${ext}`;
-  },
-  pathFor: function(folder, name, ext) {
-    return path.join(folder, this.nameFor(name, ext));
-  },
-  load: function(folder, name, ext) {
-    switch (ext) {
-      case 'yml':
-        return this.loadYaml(folder, name);
-      default:
-        // using node require to load JSON
-        return require(this.pathFor(folder, name, ext));
-    }
-  },
+export default class File {
+    constructor(name, ext = 'yml', folder = '') {
+      if (typeof name !== 'string') {
+        throw {message: 'File name must be a String'};
+      }
 
-  loadYaml: function(folder, name, ext = 'yml') {
-    try {
-      var ymlFilePath = this.pathFor(folder, name, ext);
-      var ymlFile = fs.readFileSync(ymlFilePath);
-      return yaml.safeLoad(ymlFile, 'utf8');
-    } catch (e) {
-      // TODO: add real error handler!
-      throw e;
+      if (ext && typeof ext !== 'string') {
+        throw {message: 'File ext must be a String'};
+      }
+
+      if (folder && typeof folder !== 'string') {
+        throw {message: 'Folder must be a String'};
+      }
+
+      this.folder = folder;
+      this.name = name;
+      this.ext = ext;
     }
-  }
-};
+
+    nameFor() {
+      return `${this.name}.${this.ext}`;
+    }
+
+    pathFor() {
+      return path.join(this.folder, this.nameFor());
+    }
+
+    load(folder, name, ext) {
+      switch (ext) {
+        case 'yml':
+          return this.loadYaml(this.folder, this.name);
+        default:
+          // using node require to load JSON
+          return require(this.pathFor());
+      }
+    }
+
+    loadYaml() {
+      try {
+        var ymlFilePath = this.pathFor();
+        var ymlFile = fs.readFileSync(ymlFilePath);
+        return yaml.safeLoad(ymlFile, 'utf8');
+      } catch (e) {
+        // TODO: add real error handler!
+        throw e;
+      }
+    }
+}
